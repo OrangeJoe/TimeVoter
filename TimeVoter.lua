b2b0c1046e649fadb01309545698badf48053ea9
Orange Joe <OrangeJoeServers@gmail.com>
2014-05-22 20:36:03 -0400

    Update TimeVoter.lua

--- TimeVoter.lua
+++ TimeVoter.lua
@@ -13,7 +13,6 @@ function PLUGIN:Init()
 	self:AddChatCommand( "vtime", self.cmdVoteTimer)
 	self:AddChatCommand( "vday", self.cmdVoteDay)
 	self:AddChatCommand( "vnight", self.cmdVoteNight)
-	self:TimersDestroy()
 	self.Voted = {}
 	ReqVotes = 10
 	DayTally = 0 
@@ -22,37 +21,59 @@ end
 local ReqVotes = 10
 local DayTally = 0
 
-function PLUGIN:cmdVoteDay(netuser, cmd)
-	local tme = Rust.EnvironmentControlCenter.Singleton:GetTime()
-	if tme >= tonumber(20.00) then
-		local Tally = DayTally
-		Tally = Tally + 1
-		DayTally = Tally
-		rust.BroadcastChat("=TIMEVOTER=", "A VOTE HAS BEEN CASTED FOR DAY !")
-		rust.BroadcastChat("=TIMEVOTER=", "VOTE(S) FOR DAY: " ..DayTally.. " [ " ..ReqVotes.. " ] VOTES TO PASS !")
-		timer.Once(300, function() self:cmdVoteDay() end )
-		if DayTally >= ReqVotes then
+function PLUGIN:cmdVoteDay(netuser, cmd)	-- vote for day
+	local UID = rust.GetUserID(netuser)
+	if self:CheckVote(UID) then
+		rust.SendChatToUser( netuser, "=TIMEVOTER=", "YOU HAVE ALREADY VOTED !")	-- msg if you already voted
+	else
+		local tme = Rust.EnvironmentControlCenter.Singleton:GetTime()
+		if tme >= tonumber(20.00) then
+			local Tally = DayTally
+			Tally = Tally + 1
+			DayTally = Tally
+			table.insert(self.Voted, UID)
+			rust.BroadcastChat("=TIMEVOTER=", "A VOTE HAS BEEN CASTED FOR DAY !")
+			rust.BroadcastChat("=TIMEVOTER=", "VOTE(S) FOR DAY: " ..DayTally.. " [ " ..ReqVotes.. " ] VOTES TO PASS !")
+			if DayTally >= ReqVotes then
 			rust.RunServerCommand("env.time 6")
 			rust.BroadcastChat("=TIMEVOTER=", "VOTE PASSED, NIGHT WILL BE SKIPPED !")
-			DayTally = 0			
+			DayTally = 0 		-- clears tallies
+			self.Voted = {}		-- clears voted UID table	
+			end
+		else
+			self.Voted = {}
+			rust.SendChatToUser( netuser, "=TIMEVOTER=", "POLLS OPEN FROM 20.00 - 0.00 Hrs !")	-- msg if you try and vote when votinf not actuve
 		end
-	else
-		rust.SendChatToUser( netuser, "=TIMEVOTER=", "POLLS OPEN FROM 20.00 - 0.00 Hrs !")
 	end
 end
 
-function PLUGIN:cmdVoteNight(netuser, cmd)
-	local tme = Rust.EnvironmentControlCenter.Singleton:GetTime()
-	if tme >= tonumber(20.00) then
-		local Tally = DayTally
-		Tally = Tally - 1
-		DayTally = Tally
-		rust.BroadcastChat("=TIMEVOTER=", "A VOTE HAS BEEN CASTED FOR NIGHT !")
-		rust.BroadcastChat("=TIMEVOTER=", "VOTE(S) FOR DAY: " ..DayTally.. " [ " ..ReqVotes.. " ] VOTES TO PASS !")
-		timer.Once(300, function() self:cmdVoteNight() end )
+function PLUGIN:cmdVoteNight(netuser, cmd)	-- vote for night
+	local UID = rust.GetUserID(netuser)
+	if self:CheckVote(UID) then
+		rust.SendChatToUser( netuser, "=TIMEVOTER=", "YOU HAVE ALREADY VOTED !")	-- msg if you already voted
 	else
-		rust.SendChatToUser( netuser, "=TIMEVOTER=", "POLLS OPEN FROM 20.00 - 0.00 Hrs !")
+		local tme = Rust.EnvironmentControlCenter.Singleton:GetTime()
+		if tme >= tonumber(20.00) then
+			local Tally = DayTally
+			Tally = Tally - 1
+			DayTally = Tally
+			table.insert(self.Voted, UID)
+			rust.BroadcastChat("=TIMEVOTER=", "A VOTE HAS BEEN CASTED FOR NIGHT !")
+			rust.BroadcastChat("=TIMEVOTER=", "VOTE(S) FOR DAY: " ..DayTally.. " [ " ..ReqVotes.. " ] VOTES TO PASS !")
+		else
+			self.Voted = {}
+			rust.SendChatToUser( netuser, "=TIMEVOTER=", "POLLS OPEN FROM 20.00 - 0.00 Hrs !")	-- msg if you try and vote when votinf not actuve
+		end
 	end
+end
+
+local i = 1
+
+function PLUGIN:CheckVote(UID)
+	for i = 1, #self.Voted do
+		if self.Voted[i] == UID then return true end
+	end
+	return false
 end
 
 function PLUGIN:cmdVoteTimer(netuser, cmd)
@@ -62,6 +83,6 @@ function PLUGIN:cmdVoteTimer(netuser, cmd)
 	rust.SendChatToUser( netuser, "=TIMEVOTER=", "ONE VOTE PER PLAYER | " ..ReqVotes.. " REQUIRED VOTES TO PASS")	
 end
 
-function PLUGIN:SendHelpText( netuser )
+function PLUGIN:SendHelpText(netuser)
 	rust.SendChatToUser( netuser, "/vtime | Displays info about day/night voting.")
 end
